0feed625b572fb099b11220d106c049f6b81d4aa
Ammar Saqlain <ammarsaqlain10@gmail.com>
2025-08-28 14:49:33 +0500

    choices added

--- expense.py
+++ expense.py
@@ -1,34 +1,50 @@
 import csv
+import matplotlib.pyplot as plt
 import os
 from datetime import datetime
 
-script_dir = os.path.dirname(os.path.abspath(__file__))  # folder of the script
+script_dir = os.path.dirname(os.path.abspath(__file__)) 
 file_path = os.path.join(script_dir, "expenses.csv")
 
-print("TRACK YOUR EXPENSES")
+choice = input("Do you want to add an expense Y/N: ")
 
-expense_category = input("Enter category: ")
-while True:
-    try:
-        expense_date = input("Enter the date (dd-mm-yyyy): ")
-        date_str = datetime.strptime(expense_date, "%d-%m-%Y")
-        break
-    except:
-        print("Not a valid entry for date")
-expense_amount = float(input("Enter amount: "))
+if choice.lower() == "y":
+    while True:
+        print("TRACK YOUR EXPENSES")
 
-fieldrows = [expense_category, expense_date, expense_amount]
+        expense_category = input("Enter category: ")
+        expense_information = input("Add other information: ")
 
-if os.path.exists(file_path):
-    with open(file_path, "a", newline="") as csvfile:     
-        writer = csv.writer(csvfile)
-        writer.writerow(fieldrows)
+        while True:
+            try:
+                expense_date = input("Enter the date (dd-mm-yyyy): ")
+                date_str = datetime.strptime(expense_date, "%d-%m-%Y")
+                break
+            except:
+                print("Not a valid entry for date")
+        expense_amount = float(input("Enter amount: "))
 
-else:
-    with open(file_path, "w") as csvfile:
-        fieldnames = ["Category", "Date", "Amount"]
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(fieldnames)
-    with open(file_path, "a", newline="") as csvfile:     
-        writer = csv.writer(csvfile)
-        writer.writerow(fieldrows)
+        fieldrows = [expense_category, expense_date, expense_amount, expense_information]
+
+        if os.path.exists(file_path):
+            with open(file_path, "a", newline="") as csvfile:     
+                writer = csv.writer(csvfile)
+                writer.writerow(fieldrows)
+
+        else:
+            with open(file_path, "w") as csvfile:
+                fieldnames = ["Category", "Date", "Amount", "Other info"]
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow(fieldnames)
+            with open(file_path, "a", newline="") as csvfile:     
+                writer = csv.writer(csvfile)
+                writer.writerow(fieldrows)
+
+        repeat_choice = input("Do you want to add another item Y/N: ")
+        if repeat_choice.lower() == "n":
+            break
+
+elif choice.lower() == "n":
+    print("No expense added")
+    
+print("Program Ended")
